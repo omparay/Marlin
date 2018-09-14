0dc134bf89cd2d3ddb61613859cdefc142a0f488
Unknown <omparay@gmail.com>
2018-09-13 22:03:18 -0700

    Removed S-Curve acceleration and downgraded default acceleration to 1500. Also altered advanced temperature monitoring settings.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -617,7 +617,7 @@
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
 //#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 8000, 196 }
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 152, 154, 8000, 196, 196 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 152, 158, 8000, 196, 196 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -644,9 +644,9 @@
  *   M204 R    Retract Acceleration
  *   M204 T    Travel Acceleration
  */
-#define DEFAULT_ACCELERATION          3000    //3000 X, Y, Z and E acceleration for printing moves
-#define DEFAULT_RETRACT_ACCELERATION  3000    //3000 E acceleration for retracts
-#define DEFAULT_TRAVEL_ACCELERATION   3000    //3000 X, Y, Z acceleration for travel (non printing) moves
+#define DEFAULT_ACCELERATION          1500    //3000 X, Y, Z and E acceleration for printing moves
+#define DEFAULT_RETRACT_ACCELERATION  1500    //3000 E acceleration for retracts
+#define DEFAULT_TRAVEL_ACCELERATION   1500    //3000 X, Y, Z acceleration for travel (non printing) moves
 
 /**
  * Default Jerk (mm/s)
@@ -656,9 +656,9 @@
  * When changing speed and direction, if the difference is less than the
  * value set here, it may happen instantaneously.
  */
-#define DEFAULT_XJERK                 15.0
-#define DEFAULT_YJERK                 15.0
-#define DEFAULT_ZJERK                  0.5
+#define DEFAULT_XJERK                 10.0
+#define DEFAULT_YJERK                 10.0
+#define DEFAULT_ZJERK                  0.3
 #define DEFAULT_EJERK                  5.0
 
 /**
@@ -669,7 +669,7 @@
  *
  * See https://github.com/synthetos/TinyG/wiki/Jerk-Controlled-Motion-Explained
  */
-#define S_CURVE_ACCELERATION
+//#define S_CURVE_ACCELERATION
 
 //===========================================================================
 //============================= Z Probe Options =============================
